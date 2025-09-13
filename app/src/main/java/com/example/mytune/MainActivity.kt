bdd860872211853d763eafda0a337d96ffa4305d
Francy Atehortua <fmiatehortuaj@poligran.edu.co>
2025-09-13 14:50:59 -0500

    Creacion del proyecto con la creacion de los Fragment de control de la aplicacion

---
+++ app/src/main/java/com/example/mytune/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mytune
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        replaceFragment(PerfilFragment())
+
+        val btnPerfil = findViewById<Button>(R.id.btnPerfil)
+        val btnAlbumes = findViewById<Button>(R.id.btnAlbumes)
+        val btnVideos = findViewById<Button>(R.id.btnVideos)
+        val btnStreaming = findViewById<Button>(R.id.btnStreaming)
+        val btnListas = findViewById<Button>(R.id.btnListas)
+
+        btnPerfil.setOnClickListener { replaceFragment(PerfilFragment()) }
+        btnAlbumes.setOnClickListener { replaceFragment(AlbumesFragment()) }
+        btnVideos.setOnClickListener { replaceFragment(VideosFragment()) }
+        btnStreaming.setOnClickListener { replaceFragment(StreamingFragment()) }
+        btnListas.setOnClickListener { replaceFragment(ListasFragment()) }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
